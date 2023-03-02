d08f1b40cdd0a020e0d3917f71aef55cc3c8427a
khayega <126771707+khayega@users.noreply.github.com>
2023-03-02 14:14:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+/*
+Josphine Gatwiri SIT/B/01-02268/2021
+Sheila Makena  SIT/B/01-56660/2021
+Hilda Agesa  SIT/B/01-56509/2021
+abigael Kibiwot  SIT/B/01-02282/2021
+*/
+public class Main {
+    public static void main(String[] args) {
+/*        Mmustlinkedin m = new Mmustlinkedin();
+        m.add(12);
+        m.add(13);
+        m.add(14);
+        m.add(15);
+        m.add(16);
+        m.add(17);
+        m.print();
+        m.remove(15);*/
+        /*Stack s = new Stack();
+        s.push(12);
+        s.push(13);
+        s.push(14);
+        s.push(15);
+        System.out.println(s.pop());*/
+        Queue s = new Queue();
+        s.enqueue(12);
+        s.enqueue(13);
+        s.enqueue(14);
+        s.enqueue(15);
+        System.out.println(s.isEmpty());
+    }
+}
